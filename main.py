5d81f72a6b2606c580abd212c09104ff7b4e0bec
Uwana Assam <assamuwana0@gmail.com>
2025-07-30 03:37:31 +0100

    main function and collection of courses

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    name = input("What is your name?: ") 
+    print(f"Hello {name}.Welcome to A+ VIBES! 😀 ")
+    print("Where you can confidently forecast your CGPA eventually making you an excellent student!🤩")
+    print()
+    collect_courses()
+    
+    prev_cgpa = float(input("Enter previous CGPA, if not leave blank!: ")) #Takes in input for previous CGPA 
+    prev_credits = int(input("Enter previous credit unit, if not leave blank!: ")) #Takes in input for previous credit units
+    print(f"{name}, your current CGPA is {calculate_cgpa(grades, prev_cgpa=None, prev_credits=None)}") #calls the function that would provide the CGPA
+    print(f"Our honest review and suggestions are as follows: {feedback(grades)}") #Calls the function that would give feedback 
+    
+if __name__ == "__main__":
+    main()  #calls the function that orchestrate the flow of the entire program
